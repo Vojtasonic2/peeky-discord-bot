248e6f7034cc649a4e5193716e3b83a903b3770f
Vojtasonic2 <36764258+Vojtasonic2@users.noreply.github.com>
2018-03-30 01:04:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ bot.on('message', (message) => { //Commands
 
 bot.on('message', (message) => { //About
     if(message.content == '/About'){
-       message.channel.sendMessage('Hello **' + message.author + '**! I am Peeky and I can do all kinds of amazing stuff, just type **/Commands** to see what I mean!\nIf you need any help with the bot, be sure to join the Support Server by typing **/Help**.')
+       message.channel.sendMessage('Hello **' + message.author.tag + '**! I am Peeky and I can do all kinds of amazing stuff, just type **/Commands** to see what I mean!\nIf you need any help with the bot, be sure to join the Support Server by typing **/Help**.')
        console.log('The About command has been executed. Executor: ' + message.author.tag)
     }
 })
