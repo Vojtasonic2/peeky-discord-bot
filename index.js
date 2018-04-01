262db631b2622e37ae970e614f92d1ff3a61e35e
Vojtasonic2 <36764258+Vojtasonic2@users.noreply.github.com>
2018-04-01 10:20:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ var RPS_all = [
 var globalMessage = '*No Global message has been set yet!*'
 var fishing_amount = 0
 
-bot.on('ready', () => { bot.user.setActivity('/About | ' + bot.guilds.size + ' Servers! | Version: 1.6')} )
+bot.on('ready', () => { bot.user.setActivity('bot.guilds.size + ' Servers! | 1.6')} )
 bot.on('ready', () => { bot.user.setStatus('idle')} )
 console.log('The bot is now running')
 
