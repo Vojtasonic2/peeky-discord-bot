6fe508553439b27c738f192cc0bcebad986b6b62
Vojtasonic2 <36764258+Vojtasonic2@users.noreply.github.com>
2018-04-02 21:51:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -239,10 +239,10 @@ bot.on('message', (message) => { //StartCountdown
     if(message.content.startsWith("/StartCountdown ")){
         var cntd_time = message.content.slice('16','20') + '000'
         var cntd_note = message.content.slice('21','70') + '...'
-        message.channel.sendMessage('**' + message.author.tag + "** has started a Countdown called **" + cntd_note + "** with a lenght of **" + cntd_time.slice('0','4') + " seconds**!")
+        message.channel.sendMessage('**' + message.author.tag + "** has started a **" + cntd_time.slice('0','4') + " seconds** long Countdown called **" + cntd_note + "**!")
         console.log('The StartCountdown command has been executed. Executor: ' + message.author.tag)
         setTimeout(function (){
-        message.channel.sendMessage('**' + message.author.tag + "**'s Countdown called **" + cntd_note + "** with a lenght of **" + cntd_time.slice('0','4') + " seconds** has finished!")
+        message.channel.sendMessage('**' + message.author.tag + "**'s Countdown called **" + cntd_note + "** has finished!")
         message.author.sendMessage('Hello **' + message.author.tag + '**, one of your countdowns has finished, check the server **' + message.guild.name + '** for more info!')
       }, cntd_time);
     }
