8e671a1783cd709e9868efe63f0dd5dadbd04c1b
Vojtasonic2 <36764258+Vojtasonic2@users.noreply.github.com>
2018-04-04 21:35:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -323,7 +323,7 @@ bot.on('message', (message) => { //GoFishing
     if(message.content == '/GoFishing'){
     if(message.channel.name == 'fishing'){
         var fishing_caught = fishing_fish[Math.floor(Math.random() * fishing_fish.length)];
-        message.channel.sendMessage('**' + message.author.tag + '**' + fishing_caught + '**')
+        message.channel.sendMessage('**' + message.author.tag + '**' + fishing_caught)
         fishing_amount = fishing_amount + 1
         console.log('The GoFishing command has been executed. Executor: ' + message.author.tag)
    }}
