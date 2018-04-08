3af6158a2e158de020c3505a1a054297ec796290
Vojtasonic2 <36764258+Vojtasonic2@users.noreply.github.com>
2018-04-08 20:42:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -283,7 +283,7 @@ bot.on('message', (message) => { //Commands Other
 
 bot.on('message', (message) => { //About
     if(message.content == '/About'){
-       message.channel.sendMessage("Hello, **" + message.author.tag + "**!\n\nTo get started with the bot, type **/Commands**!\n\n ● Moderation Commands\n ● Fun Commands\n ● Developer Commands\n ● Automatic Reactions in specific channels\n ● Minigames")
+       message.channel.sendMessage("Hello, **" + message.author.tag + "**!\n\nTo get started with the bot, type **/Commands**!\n\n ● Moderation Commands\n ● Fun Commands\n ● Developer Commands\n ● Automatic Reactions\n ● Minigames")
        console.log('The About command has been executed. Executor: ' + message.author.tag)
     }
 })
